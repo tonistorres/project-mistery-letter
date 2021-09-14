6c588e4b05bd183731ddbde6b9ceffaa447e9399
tonistorres <sisvenda2011@gmail.com>
2021-09-14 17:44:55 -0300

    QT 01 QT02 QT04 Respondidas e Avaliadas

--- script.js
+++ script.js
@@ -2,7 +2,43 @@
 //Repositorio Avaliativo:https://github.com/tryber/sd-016-b-project-mistery-letter/pull/2
 //Repositorio Descritivo:https://github.com/tryber/sd-016-b-project-mistery-letter
 //************************************************************************************ */
-//                          FONTE INFORMATIVA                                           /
+//                          FONTE PESQUISA                                             /
+//Documentação:w3schools.com
+//
 //************************************************************************************ */
 //Implementações:Tonis Torres
 //************************************************************************************ */
+
+let buttonGenerate = document.getElementById('criar-carta');
+
+window.onload = function () {
+
+}
+
+//Função gerar Carta 
+function generateLetter() {
+
+    let letterContent = document.getElementById('carta-texto');
+    let resultLetterContent = letterContent.value.split(" ");
+    let containerFather = document.getElementById('carta-gerada');
+
+    for (let i = 0; i < resultLetterContent.length; i += 1) {
+        let createSpan = document.createElement('span');
+        createSpan.setAttribute("class", "popular")
+        containerFather.appendChild(createSpan);
+    }
+    popularSpan();
+}
+
+function popularSpan() {
+    let captureContentSpan = document.getElementsByClassName('popular');
+    let letterContent = document.getElementById('carta-texto');
+    let resultLetterContent = letterContent.value.split(" ");
+    for (let i = 0; i < resultLetterContent.length; i++) {
+        captureContentSpan[i].innerText=resultLetterContent[i];
+    }
+}
+
+
+//Criando uma Escuta para Botaão Gerara Carta
+buttonGenerate.addEventListener("click", generateLetter);
