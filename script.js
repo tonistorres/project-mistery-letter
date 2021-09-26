b96ec349d2f8e0ea1df764119b745ee05b15f2f2
tonistorres <sisvenda2011@gmail.com>
2021-09-26 00:40:28 -0300

    Respondido QT01-Qt15 e Qt17

--- script.js
+++ script.js
@@ -1,37 +1,22 @@
-/************************************************************************************* */
-//Repositorio Avaliativo:https://github.com/tryber/sd-016-b-project-mistery-letter/pull/2
-//Repositorio Descritivo:https://github.com/tryber/sd-016-b-project-mistery-letter
-//************************************************************************************ */
-//                          FONTE PESQUISA                                             /
-//Documentação:w3schools.com
-//
-//************************************************************************************ */
-//Implementações:Tonis Torres
-//************************************************************************************ */
+// Repositorio Avaliativo: https: //github.com/tryber/sd-016-b-project-mistery-letter/pull/2
+// Repositorio Descritivo: https: //github.com/tryber/sd-016-b-project-mistery-letter
+// FONTE PESQUISA
+// Implementações: Tonis Torres
 
-let buttonGenerate = document.getElementById('criar-carta');
-let letterContent = document.getElementById('carta-texto');
-let generatedLetter;
+const buttonGenerate = document.querySelector('#criar-carta');
+const letterContent = document.querySelector('#carta-texto');
 
-window.onload = function () {
-
-}
-
-//Função gerar Carta 
+// eslint-disable-next-line no-unused-vars
 function generateLetter() {
-
     let letterContent = document.getElementById('carta-texto');
     let resultLetterContent = letterContent.value.split(" ");
     let containerFather = document.getElementById('carta-gerada');
-
     for (let i = 0; i < resultLetterContent.length; i += 1) {
         let createSpan = document.createElement('span');
         createSpan.setAttribute("class", "popular")
         containerFather.appendChild(createSpan);
     }
     popularSpan();
-
-
 }
 
 function popularSpan() {
@@ -43,24 +28,48 @@ function popularSpan() {
     }
 }
 
-
-//Função de validação
 function validationLetter(str) {
-    let input = document.getElementById('carta-texto');
-    if (str.value === ""|| str.value>=1!==" ") {
-        input.innerText = "Por favor, digite o conteúdo da carta.";
-        input.focus();
-        input.style.background = "yellow";
-        document.getElementById('carta-gerada').innerText="Por favor, digite o conteúdo da carta.";
+    //Documentação:https://www.w3schools.com/jsref/jsref_trim_string.asp
+    //Remova os espaços em branco de ambos os lados de uma string:
+    const filterString = str.value.trim();
+    if ((filterString === "") || (filterString === " ")) {
+        estilizaCriaUmaBarraInfo();
+    } else {
+        criarContainerPhrases(filterString);
     }
+
 }
 
-//Criando uma Escuta Função de Validação
-buttonGenerate.addEventListener("click", function (evt) {
+function estilizaCriaUmaBarraInfo() {
+    alert('função estilizar')
+    let response = document.querySelector('#carta-gerada');
+    let input = document.querySelector('#carta-texto');
+    input.focus();
+    input.value = "";
+    input.style.background = "yellow";
+    response.innerText = "Por favor, digite o conteúdo da carta.";
+
+}
+
+
+function criarContainerPhrases(str) {
+    //Documentação:https://www.w3schools.com/jsref/jsref_trim_string.asp
+    //Remova os espaços em branco de ambos os lados de uma string:
+    let input = document.querySelector('#carta-texto');
+    const phrasesArray = str.split(" ");
+    let containerPhrasesGenerated = document.querySelector('#carta-gerada');
+    containerPhrasesGenerated.innerHTML = "";
+    for (let i = 0; i < phrasesArray.length; i += 1) {
+        let createSpan = document.createElement('span');
+        createSpan.innerText = phrasesArray[i];
+        containerPhrasesGenerated.appendChild(createSpan);
+    }
+    input.style.background = "white";
+
+}
+
+buttonGenerate.addEventListener("click", (evt) => {
+    //Documentação:https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
+    evt.preventDefault();
     validationLetter(letterContent);
 });
-
-
-
-
-
